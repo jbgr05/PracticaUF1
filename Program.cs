48c301088e96c2d7a80b8e0c688aae170a17e519
jbgr05 <74369941+jbgr05@users.noreply.github.com>
2023-11-03 11:51:24 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numero1;
+        int numero2;
+        int resultado;
+        Console.WriteLine("Dime un numero: ");
+        numero1=Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Dime otro numero: ");
+        numero2=Convert.ToInt32(Console.ReadLine());
+
+        resultado=numero1+numero2;
+
+        Console.WriteLine("La suma de {0} + {1} da un resultado de {2}", numero1,numero2,resultado);
+    }
+}
